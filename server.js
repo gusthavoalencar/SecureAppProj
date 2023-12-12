642bbd75ad5d412346347de67787324142b4cfb4
Gusthavo Alencar <56687423+gusthavoalencar@users.noreply.github.com>
2023-12-12 21:25:15 +0000

    index html & api request

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from gus!');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/test', (req, res) => {
+  res.send({ some: 'Hello frasfoddm gus!' });
 });
 
 app.listen(3000, () => {
